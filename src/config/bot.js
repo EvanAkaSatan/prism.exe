356f7f8300045fbc60b6fe832b2af5b3f9cc408b
EvanAkaSatan <79179693+EvanAkaSatan@users.noreply.github.com>
2026-05-18 22:50:11 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -10,7 +10,7 @@ export const botConfig = {
   // - "idle"      = yellow moon
   // - "dnd"       = red do-not-disturb
   // - "invisible" = appears offline
-  presence: {
+  presence: {"counting the points..."
     // Current online state shown on Discord.
     status: "online",
 
