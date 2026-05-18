3dfbf7ede2d20359fd4829a39eb94160c29ca771
EvanAkaSatan <79179693+EvanAkaSatan@users.noreply.github.com>
2026-05-18 22:47:38 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "supervising battles . . . 🦇🌊🪽",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 5, 
       },
     ],
   },
